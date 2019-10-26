f999c04e3deb0fc45cdd775733c73f52ecba066c
BosSaksit <saksit.kku@gmail.com>
2019-10-26 15:16:40 +0700

    set allow

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace demoAapi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(builder => builder.WithOrigins("*").AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader());
             app.UseHttpsRedirection();
             app.UseSwagger();
 
